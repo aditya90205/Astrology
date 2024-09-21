c884151f16236845057970bdc42f354e72579c4a
Moracus <hsharshsharma370@gmail.com>
2024-09-21 13:12:41 +0530

    added astrologer model and routes

--- Backend/models/sp_astrologer_master.js
+++ Backend/models/sp_astrologer_master.js
@@ -5,14 +5,56 @@ const astrologerSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  expertise: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Expertise",
-  },
-  lang: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Language",
-  },
+  expertise: [
+    {
+      type: String,
+      enum: [
+        "Vedic",
+        "Numerology",
+        "KP System",
+        "Lal Kitab",
+        "Horary",
+        "Vastu",
+        "Tarot Reading",
+        "Love Life",
+        "Nadi",
+        "Marriage Matching",
+        "Ashtakavarga",
+        "Palmistry",
+        "Ramal",
+        "Jaimini",
+        "Tajik",
+        "Western",
+        "South Astrology",
+        "Ravan Sahita",
+        "Swar Shastra",
+        "Reiki",
+        "Crystal Healing",
+        "Angel Reading",
+        "Pendulum Dowsing",
+        "Psychic Reading",
+        "Face Reading",
+        "Muhurat",
+      ],
+    },
+  ],
+  language: [
+    {
+      type: String,
+      enum: [
+        "English",
+        "Hindi",
+        "Gujarati",
+        "Marathi",
+        "Tamil",
+        "Telugu",
+        "Kannada",
+        "Malayalam",
+        "Bengali",
+        "Punjabi",
+      ],
+    },
+  ],
   experience: {
     type: Number,
   },
@@ -29,10 +71,18 @@ const astrologerSchema = new mongoose.Schema({
   rating: {
     type: Number,
   },
+  ratingCount: {
+    type: Number,
+    default: 0, // Set default to 0 since initially there will be no ratings
+  },
   followers: {
     type: Number,
   },
   status: {
     type: Number,
+    enum: [0, 1, 2],
   },
 });
+
+const Astrologer = mongoose.model("sp_astrologer_master", astrologerSchema);
+export default Astrologer;
